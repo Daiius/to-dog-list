e82aea429f1b21ff8817c51092cf046e66919fd7
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-05-25 10:40:56 +0900

    TaskInpuコンポーネントの調整
    補完時に背景色や文字サイズが変化してしまうが、
    これに対処するのは難しそうだったのでautoComplete機能を無効にした
    
    スタイル適応による対処も試してはみたが効果がなさそう...

--- src/components/base/Input.tsx
+++ src/components/base/Input.tsx
@@ -11,7 +11,7 @@ export type InputProps =
 	& React.ComponentProps<typeof HeadlessInput>
 	& Omit<
 			React.ComponentProps<'input'>,
-			'value' | 'onChange'
+			'onChange'
 		>
 	& {
 		onChange: (newValue: string) => void;
@@ -23,26 +23,32 @@ const Input: React.FC<InputProps> = ({
 	onChange,
 	...props
 }) => (
-	<div className='flex flex-row'>
-		<HeadlessInput
+	<div
+		className={clsx(
+			'flex flex-row', props.className
+		)}
+	>
+		<input
 			{...props}
 			value={value}
 			onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(e.target.value)}
 			className={clsx(
 				'border border-1 border-slate-300 bg-white',
-				'rounded-md p-2',
+				'rounded-md p-2 w-full',
 				'dark:bg-slate-500 dark:border-slate-800',
 				'focus:outline-none',
-				props.className,
+				'autofill:!bg-slate-300',
 			)}
 		/>
-		{value &&
-			<HeadlessButton
-				onClick={() => onChange("")}
-			>
-				<i className='bi bi-x relative right-0'/>
-			</HeadlessButton>
-		}
+		<HeadlessButton
+			className={clsx(
+				'relative -ml-5',
+				value.length > 0 ? 'visible' : 'invisible'
+			)}
+			onClick={() => onChange("")}
+		>
+			<i className='bi bi-x'/>
+		</HeadlessButton>
 	</div>
 );
 
--- src/components/case/TaskInput.tsx
+++ src/components/case/TaskInput.tsx
@@ -1,24 +1,50 @@
 'use client';
 
 import React from 'react';
+import clsx from 'clsx';
 import Input from '@/components/base/Input';
-import { Field, Label } from '@headlessui/react';
+import { 
+	Field,
+	Label,
+	Button as HeadlessButton
+} from '@headlessui/react';
 
 const TaskInput: React.FC = () => {
 	const [newTask, setNewTask] = React.useState<string>("");
+
+	const handleSubmitTask = () => {
+		console.log(`${newTask} is submitted!`);
+		setNewTask("");
+	};
+
 	return (
-		<Field>
+		<Field >
 			<Label>New To-Do:</Label>
-			<Input 
-				value={newTask} 
-				onChange={(newValue: string) => setNewTask(newValue)}
-				onKeyDown={(e: React.KeyboardEvent) => {
-					if (!e.nativeEvent.isComposing && e.key === 'Enter') {
-						console.log(`${newTask} is submitted!`);
-						setNewTask("");
-					}
-				}}
-			/>
+			<div className='flex flex-row'>
+				<i className='bi bi-pen text-xl self-center mr-2'/>
+				<Input 
+					value={newTask} 
+					onChange={(newValue: string) => setNewTask(newValue)}
+					onKeyDown={(e: React.KeyboardEvent) => {
+						if (!e.nativeEvent.isComposing && e.key === 'Enter') {
+							handleSubmitTask();
+						}
+					}}
+					className='w-full'
+				/>
+				<HeadlessButton
+					className={clsx(
+						'border-[1.5px] dark:border-white',
+						'rounded-md h-[80%] self-center ml-2 px-2',
+						newTask.length > 0 ? 'visible' : 'invisible'
+					)}
+					onClick={handleSubmitTask}
+				>
+					<i className={clsx(
+						'bi bi-arrow-return-left self-center text-lg',
+					)}/>
+				</HeadlessButton>
+			</div>
 		</Field>
 	);
 };
